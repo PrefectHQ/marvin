d302517e59a75515512744cefe947969206e4ad6
Nathan Nowack <thrast36@gmail.com>
2023-03-27 18:33:47 -0500

    add confirm for destructive things

--- src/marvin/cli/db.py
+++ src/marvin/cli/db.py
@@ -8,15 +8,42 @@ database_app = typer.Typer(help="Database commands")
 
 
 @database_app.command()
-def create_db():
-    asyncio.run(marvin.infra.db.create_db())
+def create(
+    confirm: bool = typer.Option(
+        False,
+        prompt="Are you sure you want to create the database?",
+        help="Confirm before creating the database.",
+    )
+):
+    if confirm:
+        asyncio.run(marvin.infra.db.create_db())
+    else:
+        typer.echo("Database creation cancelled.")
 
 
 @database_app.command()
-def destroy_db():
-    asyncio.run(marvin.infra.db.destroy_db())
+def destroy(
+    confirm: bool = typer.Option(
+        False,
+        prompt="Are you sure you want to destroy the database?",
+        help="Confirm before destroying the database.",
+    )
+):
+    if confirm:
+        asyncio.run(marvin.infra.db.destroy_db())
+    else:
+        typer.echo("Database destruction cancelled.")
 
 
 @database_app.command()
-def reset_db():
-    asyncio.run(marvin.infra.db.reset_db())
+def reset(
+    confirm: bool = typer.Option(
+        False,
+        prompt="Are you sure you want to reset the database?",
+        help="Confirm before resetting the database.",
+    )
+):
+    if confirm:
+        asyncio.run(marvin.infra.db.reset_db())
+    else:
+        typer.echo("Reset cancelled.")
