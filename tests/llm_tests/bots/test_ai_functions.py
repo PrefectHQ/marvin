6dd2d51c4779506cab0d849f2badde92ddbf2878
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2023-03-28 20:17:53 -0400

    Add test

--- tests/llm_tests/bots/test_ai_functions.py
+++ tests/llm_tests/bots/test_ai_functions.py
@@ -109,6 +109,18 @@ class TestBool:
         result = classify_sentiment(["i love pizza", "i hate pizza"])
         assert result == [True, False]
 
+    def test_extract_sentences_with_question_mark(self):
+        @ai_fn
+        def list_questions(email_body: str) -> list[str]:
+            """
+            return any sentences that end with a question mark found in the
+            email_body
+            """
+
+        email_body = "Hi Taylor, It is nice outside today. What is your favorite color?"
+        x = list_questions(email_body)
+        assert x == ["What is your favorite color?"]
+
 
 class TestSet:
     def test_set_response(self):
