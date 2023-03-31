321acfaf00113245864288b3416f817a76b47c1d
Jeff Hale <jeffmshale@gmail.com>
2023-03-31 10:43:43 -0400

    add joke generator

---
+++ examples/simple-examples/joke.py
@@ -0,0 +1,7 @@
+@ai_fn
+def joke_of_the_day() -> list[str]:
+    """Generates a random punny joke of the day"""
+
+print(joke_of_the_day())
+
+# Why did the tomato turn red? because it saw the salad dressing!
