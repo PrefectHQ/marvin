2aba5f10e763d9567ee1e25232b07b29b5dcca94
Nathan Nowack <thrast36@gmail.com>
2023-04-09 19:12:14 -0500

    so wild

--- tests/utilities/test_llm_utils.py
+++ tests/utilities/test_llm_utils.py
@@ -1,3 +1,6 @@
+import datetime
+import platform
+
 import pytest
 from marvin.models.threads import Message
 from marvin.utilities.llms import trim_to_context_window
@@ -102,6 +105,21 @@ from marvin.utilities.llms import trim_to_context_window
     ],
 )
 def test_trim_context_window(messages, max_tokens, expected_result):
+    # TIL that Windows has a different timestamp precision than unix systems
+    if platform.system() == "Windows":
+        messages = [
+            Message(
+                **msg.dict(), timestamp=msg.timestamp + datetime.timedelta(seconds=i)
+            )
+            for i, msg in enumerate(messages)
+        ]
+        expected_result = [
+            Message(
+                **msg.dict(), timestamp=msg.timestamp + datetime.timedelta(seconds=i)
+            )
+            for i, msg in enumerate(expected_result)
+        ]
+
     result = trim_to_context_window(messages, max_tokens)
 
     excluded_attrs = {"id", "timestamp", "bot_id", "data", "thread_id"}
