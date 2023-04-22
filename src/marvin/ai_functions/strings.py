5d53a4d1dfdb3a567bf27df580bc6ae842dc6ba9
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2023-04-22 10:26:15 -0400

    Add entity functions

--- src/marvin/ai_functions/strings.py
+++ src/marvin/ai_functions/strings.py
@@ -2,7 +2,15 @@ from marvin.ai_functions import ai_fn
 
 
 @ai_fn
-def title_case(input: str) -> str:
+def fix_capitalization(text: str) -> str:
     """
-    Given a string {input} change the case to make it APA style guide title case.
+    Given `text`, fix any capitalization errors. Do not change the text in any
+    other way.
+    """
+
+
+@ai_fn
+def title_case(text: str) -> str:
+    """
+    Given `text`, change the case to make it APA style guide title case.
     """
--- tests/llm_tests/ai_functions/test_strings.py
+++ tests/llm_tests/ai_functions/test_strings.py
@@ -1,10 +1,20 @@
 from marvin.ai_functions import strings as string_fns
 
 
+class TestFixCapitalization:
+    def test_fix_capitalization(self):
+        result = string_fns.fix_capitalization("the european went over to canada, eh?")
+        assert result == "The European went over to Canada, eh?"
+
+
 class TestTitleCase:
+    def test_title_case(self):
+        result = string_fns.title_case("the european went over to canada, eh?")
+        assert result == "The European Went Over to Canada, Eh?"
+
     def test_short_prepositions_not_capitalized(self):
         result = string_fns.title_case(
             input="let me go to the store",
         )
 
-        assert result == "Let Me Go to the store"
+        assert result == "Let Me Go to the Store"
