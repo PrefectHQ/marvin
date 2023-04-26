47b3e87e80b6e6fdb6c9b8810b70521ff19fff48
nate nowack <thrast36@gmail.com>
2023-04-26 10:32:53 -0500

    wording

--- src/marvin/plugins/prefect_stuff.py
+++ src/marvin/plugins/prefect_stuff.py
@@ -13,7 +13,7 @@ from marvin.utilities.strings import slice_tokens
 @plugin
 async def review_flow_run(flow_run_name: str, token_limit: int = 3000) -> str:
     """Retrieve and inspect logs from a Prefect flow run. The `flow_run_name`
-    will always be `adjective-animal`.
+    will always be of the form `adjective-animal`.
     """
     async with get_client() as client:
         flow_runs = await client.read_flow_runs(
