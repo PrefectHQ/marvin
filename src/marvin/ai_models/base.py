534dc7b23f56f189e6e8839a651a0ea38b554c21
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2023-05-05 19:12:36 -0400

    formtting

--- src/marvin/ai_models/base.py
+++ src/marvin/ai_models/base.py
@@ -7,7 +7,8 @@ from marvin import ai_fn
 from marvin.bot import Bot
 from marvin.bot.response_formatters import TypeFormatter
 
-M = TypeVar('M', bound=pydantic.BaseModel)
+M = TypeVar("M", bound=pydantic.BaseModel)
+
 
 def AIModel(
     cls: Optional[Type[M]],
@@ -15,54 +16,52 @@ def AIModel(
     bot: Bot = None,
     **bot_kwargs,
 ) -> Type[M]:
-    
     def __unstructured_context_handler__(func):
-        # This decorator is used to handle a single positional `context` argument in the __init__ method.
+        # wrapper to handle a single positional `str` in the __init__ method.
         @wraps(func)
         def wrapper(self, *args, **kwargs):
             context = {}
             # If the first argument is a string, we assume it is the context.
             if next(iter(args), None) and isinstance(next(iter(args), None), str):
-                context = {'__marvin_context__': args[0]}
-            result = func(self, **{**context, **kwargs})
+                context = {"__marvin_context__": args[0]}
+            func(self, **{**context, **kwargs})
+
         return wrapper
-    
 
     def _ai_imputer_base(context: str) -> cls:
-        '''
-            Given unstructured text infer when possible the missing data.
-        '''
-    
+        """
+        Given unstructured text infer when possible the missing data.
+        """
+
     def _ai_validator(cls, values):
         # Check if a __marvin_context__ has been passed in the init method.
-        __context__ = values.pop('__marvin_context__', None)
-        # If a __marvin_context__ has been passed, we'll use an ai_fn to impute the values.
+        __context__ = values.pop("__marvin_context__", None)
+        # If __marvin_context__ has been passed, use an ai_fn to impute the values.
         if __context__:
-            # Here we use the ai_fn decorator to create a function that will be used to impute the values.
-            ai_imputer = (
-                ai_fn(
-                    _ai_imputer_base, 
-                    bot = bot, 
-                    bot_kwargs = {
-                        **bot_kwargs,
-                        'response_format': TypeFormatter(type_schema = cls.schema())
-                    }
-                )
+            # use ai_fn  to create a function that will be used to impute the values.
+            ai_imputer = ai_fn(
+                _ai_imputer_base,
+                bot=bot,
+                bot_kwargs={
+                    **bot_kwargs,
+                    "response_format": TypeFormatter(type_schema=cls.schema()),
+                },
             )
             # We'll attempt to impute the values using the ai_imputer function.
-            try: 
-                return({**ai_imputer(context = __context__).dict(), **values})
+            try:
+                return {**ai_imputer(context=__context__).dict(), **values}
             # If the ai_imputer function fails, we'll simple return the values.
-            except Exception as e: #TODO: Add a custom exception here.
-                return(values)
+            except Exception:  # TODO: Add a custom exception here.
+                return values
         return values
 
-    # We'll replace the __init__ method with our custom __unstructured_context_handler__ decorator.
+    # wrap the __init__ method in __unstructured_context_handler__
     cls.__init__ = __unstructured_context_handler__(cls.__init__)
 
-    # We'll add the _ai_validator as a pre root validator to run before any other root validators.
+    # add _ai_validator as a pre root validator to run before any other root validators.
     cls.__pre_root_validators__ = [_ai_validator, *cls.__pre_root_validators__]
     return cls
+
 
 def ai_model(
     cls: Optional[Type[M]] = None,
@@ -74,6 +73,3 @@ def ai_model(
     if cls is None:
         return partial(ai_model, bot=bot, **bot_kwargs)
     return AIModel(cls=cls, bot=bot, bot_kwargs=bot_kwargs)
-
-
-
--- tests/llm_tests/ai_models/test_ai_models.py
+++ tests/llm_tests/ai_models/test_ai_models.py
@@ -8,125 +8,132 @@ class TestAIModels:
     def test_arithmetic(self):
         @ai_model
         class Arithmetic(pydantic.BaseModel):
-            value: float 
+            value: float
             is_odd: bool
+
         x = Arithmetic("One plus six")
         assert x.value == 7
         assert x.is_odd
-        
+
     def test_geospatial(self):
         @ai_model
         class Location(pydantic.BaseModel):
             latitude: float
-            longitude: float 
-            city: str 
+            longitude: float
+            city: str
             state: str
             country: str
-        
-        x = Location('The largest city in the Sunshine State.')
-        assert x.city == 'Miami'
-        assert x.state == 'Florida'
-        assert x.country.str.contains('United')
-        assert x.country.str.contains('States')
-        assert x.latitude//1 == 25
-        assert x.longitude//1 == -81
-        
+
+        x = Location("The largest city in the Sunshine State.")
+        assert x.city == "Miami"
+        assert x.state == "Florida"
+        assert x.country.str.contains("United")
+        assert x.country.str.contains("States")
+        assert x.latitude // 1 == 25
+        assert x.longitude // 1 == -81
+
     def test_regex(self):
         @ai_model
         class Acronym(pydantic.BaseModel):
             value: str = pydantic.Field(regex=r"Re[a-z]+ L[a-z]+ L[a-z]+ MIa-z]+")
+
         Acronym("ReLLM is one way to get structured data out of LLMs")
-        
-        
+
     def test_depth(self):
         from typing import List
+
         class Country(pydantic.BaseModel):
             name: str
-        
+
         class City(pydantic.BaseModel):
             name: str
             country: Country
-        
+
         class Neighborhood(pydantic.BaseModel):
-            name: str 
+            name: str
             city: City
-                
-        @ai_model        
+
+        @ai_model
         class RentalHistory(pydantic.BaseModel):
             neighborhood: List[Neighborhood]
-                
-        return(RentalHistory('''\
+
+        return RentalHistory("""\
             I lived in Palms, then Mar Vista, then Pico Robertson.
-        '''))
-        
+        """)
+
     def test_resume(self):
         class Experience(pydantic.BaseModel):
             technology: str
             years_of_experience: int
             supporting_phrase: Optional[str]
-                
+
         @ai_model
         class Resume(pydantic.BaseModel):
             has_three_years_management_experience: bool
             has_ten_years_management_experience: bool
             technologies: List[Experience]
-                               
-        x = Resume('''\
+
+        x = Resume("""\
             Data Engineering Manager, 2017-2022
             • Managed team of three engineers and data scientists
             • Deployed and maintained internal Apache Kafka pipeline
             • Built tree-based classifier to predict customer churn (xgboost)\
-        ''')
-        
+        """)
+
         assert x.has_three_years_management_experience
         assert not x.has_ten_years_management_experience
         assert len(x.technologies) == 2
-        
+
     def test_literal(self):
         class Person(pydantic.BaseModel):
-            name: Literal['Adam', 'Nate', 'Jeremiah']
-        
+            name: Literal["Adam", "Nate", "Jeremiah"]
+
         @ai_model
         class Conversation(pydantic.BaseModel):
             speakers: List[Person]
-        
-        x = Conversation('''\
+
+        x = Conversation("""\
             The conference for best LLM framework will feature talks by\
             Adam, Nate, Jeremiah, and Marvin.\
-        ''')
+        """)
         assert len(x.speakers) == 3
-        assert set([
-            speaker.name for speaker in x.speakers
-        ]) == set(['Adam', 'Nate', 'Jeremiah'])
-  
+        assert set([speaker.name for speaker in x.speakers]) == set(
+            ["Adam", "Nate", "Jeremiah"]
+        )
+
     def test_history(self):
         from typing import List
+
         class Location(pydantic.BaseModel):
             city: str
             state: str
-    
+
         class Candidate(pydantic.BaseModel):
-            name: str 
+            name: str
             political_party: str
             campaign_slogan: str
             birthplace: Location
-        
+
         @ai_model
         class Election(pydantic.BaseModel):
             winner: Candidate
             candidates: List[Candidate]
-                
-        x = Election('The United States Election of 1800')
-        
-        assert x.winner in x.candidates 
-        assert x.winner.name == 'Thomas Jefferson'
-        assert x.winner.political_party == 'Democratic-Republican'
-        assert x.winner.birthplace.city == 'Shadwell'
-        assert x.winner.birthplace.state == 'Virginia'
-        assert set([candidate.name for candidate in x.candidates]).issubset(set([
-            'Thomas Jefferson',
-            'John Adams',
-            'Aaron Burr',
-            'Charles C. Pinckney',
-            'Charles Pinckney'
-        ]))
+
+        x = Election("The United States Election of 1800")
+
+        assert x.winner in x.candidates
+        assert x.winner.name == "Thomas Jefferson"
+        assert x.winner.political_party == "Democratic-Republican"
+        assert x.winner.birthplace.city == "Shadwell"
+        assert x.winner.birthplace.state == "Virginia"
+        assert set([candidate.name for candidate in x.candidates]).issubset(
+            set(
+                [
+                    "Thomas Jefferson",
+                    "John Adams",
+                    "Aaron Burr",
+                    "Charles C. Pinckney",
+                    "Charles Pinckney",
+                ]
+            )
+        )
