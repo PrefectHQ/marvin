9ac1e051807c12de49498123507ea830d94d3d25
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2023-05-05 18:22:13 -0400

    ai_models for structured parsing
    ai_model is a decorator for pydantic models that allows you to pass unstructured context to a pydantic model for destructing or inference.

---
+++ src/marvin/ai_models/base.py
@@ -0,0 +1,79 @@
+from functools import partial, wraps
+from typing import Optional, Type, TypeVar
+
+import pydantic
+
+from marvin import ai_fn
+from marvin.bot import Bot
+from marvin.bot.response_formatters import TypeFormatter
+
+M = TypeVar('M', bound=pydantic.BaseModel)
+
+def AIModel(
+    cls: Optional[Type[M]],
+    *,
+    bot: Bot = None,
+    **bot_kwargs,
+) -> Type[M]:
+    
+    def __unstructured_context_handler__(func):
+        # This decorator is used to handle a single positional `context` argument in the __init__ method.
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            context = {}
+            # If the first argument is a string, we assume it is the context.
+            if next(iter(args), None) and isinstance(next(iter(args), None), str):
+                context = {'__marvin_context__': args[0]}
+            result = func(self, **{**context, **kwargs})
+        return wrapper
+    
+
+    def _ai_imputer_base(context: str) -> cls:
+        '''
+            Given unstructured text infer when possible the missing data.
+        '''
+    
+    def _ai_validator(cls, values):
+        # Check if a __marvin_context__ has been passed in the init method.
+        __context__ = values.pop('__marvin_context__', None)
+        # If a __marvin_context__ has been passed, we'll use an ai_fn to impute the values.
+        if __context__:
+            # Here we use the ai_fn decorator to create a function that will be used to impute the values.
+            ai_imputer = (
+                ai_fn(
+                    _ai_imputer_base, 
+                    bot = bot, 
+                    bot_kwargs = {
+                        **bot_kwargs,
+                        'response_format': TypeFormatter(type_schema = cls.schema())
+                    }
+                )
+            )
+            # We'll attempt to impute the values using the ai_imputer function.
+            try: 
+                return({**ai_imputer(context = __context__).dict(), **values})
+            # If the ai_imputer function fails, we'll simple return the values.
+            except Exception as e: #TODO: Add a custom exception here.
+                return(values)
+        return values
+
+    # We'll replace the __init__ method with our custom __unstructured_context_handler__ decorator.
+    cls.__init__ = __unstructured_context_handler__(cls.__init__)
+
+    # We'll add the _ai_validator as a pre root validator to run before any other root validators.
+    cls.__pre_root_validators__ = [_ai_validator, *cls.__pre_root_validators__]
+    return cls
+
+def ai_model(
+    cls: Optional[Type[M]] = None,
+    *,
+    bot: Bot = None,
+    **bot_kwargs,
+) -> Type[M]:
+    # this allows the decorator to be used with or without calling it
+    if cls is None:
+        return partial(ai_model, bot=bot, **bot_kwargs)
+    return AIModel(cls=cls, bot=bot, bot_kwargs=bot_kwargs)
+
+
+
