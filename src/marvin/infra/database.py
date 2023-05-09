b951bc17846ec6e88da0cce3479b815a3bc1c4ca
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2023-05-09 00:00:32 -0400

    init

--- src/marvin/infra/database.py
+++ src/marvin/infra/database.py
@@ -212,7 +212,7 @@ def alembic_revision(message: str = None, autogenerate: bool = False, **kwargs):
     )
 
 
-async def check_alembic_version():
+def check_alembic_version():
     # get current alembic version as scalar
     output_buffer = io.StringIO()
     alembic_cfg = _alembic_cfg(stdout=output_buffer)
@@ -276,3 +276,6 @@ async def check_alembic_version():
                 ),
                 DatabaseWarning,
             )
+
+async def acheck_alembic_version():
+    check_alembic_version()
