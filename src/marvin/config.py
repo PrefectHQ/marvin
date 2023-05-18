f5b43310ca6ad8a52f898f8486dd3f0c25c1d07d
nate nowack <thrast36@gmail.com>
2023-05-18 13:38:39 -0500

    typo

--- src/marvin/config.py
+++ src/marvin/config.py
@@ -100,7 +100,7 @@ class Settings(BaseSettings):
         "", env=["MARVIN_OPENAI_API_KEY", "OPENAI_API_KEY"]
     )
     llm_request_timeout_seconds: Union[float, List[float], None] = Field(
-        60.0, env=["MARVIN_LLM_REQUEST_TIMEOUT_SETTING"]
+        600.0, env=["MARVIN_LLM_REQUEST_TIMEOUT_SETTING"]
     )
 
     # CHROMA
