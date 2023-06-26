4e11276c4cc3ecd8586f9b3941570dd0897e9996
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2023-06-26 17:07:32 -0400

    ruff settings --adams-neuroticism

--- src/marvin/__init__.py
+++ src/marvin/__init__.py
@@ -1,8 +1,4 @@
-from .settings import Settings
-
-settings = Settings()
-
+from .settings import settings
 from .primitives import ai_fn, ai_application, ai_model, ai_model_factory
-
 
 __all__ = ["ai_fn", "ai_application", "ai_model", "ai_model_factory", "settings"]
