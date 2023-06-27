83596bd226e49a0c5683b2be526cce98aff40f51
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2023-06-27 10:40:48 -0400

    agent: kwargs -> route_kwargs

--- src/marvin/agents/base.py
+++ src/marvin/agents/base.py
@@ -1,20 +1,20 @@
-from typing import Any, Callable, List, Type, Union
+from typing import Callable
 
 from fastapi import APIRouter
 from fastapi.routing import APIRouter
 from marvin.engines.language_models import ChatLLM
-from marvin.prompts import Prompt, System, User
-from pydantic import BaseModel, Extra, root_validator, validator
+from marvin.prompts import Prompt
+from pydantic import BaseModel, Extra
 
 
 class FunctionRegistry(APIRouter):
-    def attach(self, functions: list[Callable], **kwargs):
+    def attach(self, functions: list[Callable], **route_kwargs):
         for function in functions:
             self.add_api_route(**{
                 'methods': ['POST'],
                 'path': f'/tools/{function.__name__}',
                 'description': function.__doc__,
-                **kwargs,
+                **route_kwargs,
                 'endpoint': function
             })
         
@@ -42,11 +42,11 @@ class Agent(
         self._router.include_router(self._function_registry)
         return(self._router)
     
-    def register(self, *args, **kwargs):
+    def register(self, *args, **route_kwargs):
         def decorator(func):
             def wrapper(*tool):
                 return func(*tool) 
-            self._function_registry.attach(functions = [func], **kwargs)
+            self._function_registry.attach(functions = [func], **route_kwargs)
             self.functions = [route.endpoint for route in self._function_registry.routes]
             return wrapper
         return decorator
