8771d1cc5cfff72c0f5de6cbc805c2760faa0d75
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2023-06-26 20:15:32 -0400

    ai_model_factory

--- src/marvin/primitives/ai_model_factory/__init__.py
+++ src/marvin/primitives/ai_model_factory/__init__.py
@@ -1,1 +1,17 @@
-default = ""
+import asyncio
+import functools
+from typing import Optional, Type, TypeVar
+
+from pydantic import BaseModel
+
+from marvin.models.meta import DataSchema
+from marvin.primitives.ai_model.ai_model import ai_model
+
+T = TypeVar("T")
+
+# If you're reading this and expected something fancier, 
+# I'm sorry to disappoint you. It's this simple.
+
+ai_model_factory = ai_model(DataSchema)
+
+default = ai_model_factory
