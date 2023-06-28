3f405fe3add4bfa4251b8d00961a4d0f85272dac
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2023-06-28 19:09:48 -0400

    AIModel, AIFunction top level exports

--- src/marvin/__init__.py
+++ src/marvin/__init__.py
@@ -1,4 +1,4 @@
 from .settings import settings
-from .primitives import ai_fn, AIApplication, ai_model, AIModelFactory
+from .primitives import ai_fn, AIApplication, ai_model, AIModelFactory, AIFunction, AIModel
 
-__all__ = ["ai_fn", "AIApplication", "ai_model", "AIModelFactory", "settings"]
+__all__ = ["ai_fn", "AIApplication", "ai_model", "AIModelFactory", "settings", "AIFunction", "AIModel"]
