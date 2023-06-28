cd5170766de4df6832a7a8f6165d9834b5b33105
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2023-06-28 15:21:27 -0400

    agents update

--- src/marvin/agents/base.py
+++ src/marvin/agents/base.py
@@ -2,23 +2,11 @@ from typing import Callable
 
 from fastapi import FastAPI
 from fastapi.routing import APIRouter
+from marvin.agents.flows import Flow, OpenAIFlow
 from marvin.engines.language_models import ChatLLM
+from marvin.functions.base import FunctionRegistry
 from marvin.prompts import Prompt
 from pydantic import BaseModel, Extra
-
-
-class FunctionRegistry(APIRouter):
-    def attach(self, functions: list[Callable], **route_kwargs):
-        for function in functions:
-            self.add_api_route(
-                **{
-                    "methods": ["POST"],
-                    "path": f"/tools/{function.__name__}",
-                    "description": function.__doc__,
-                    **route_kwargs,
-                    "endpoint": function,
-                }
-            )
 
 
 class Agent(
@@ -30,17 +18,21 @@ class Agent(
     functions: list[Callable] = []
 
     _app: FastAPI = FastAPI()
-    _router: APIRouter = APIRouter()
-    _function_registry: FunctionRegistry = FunctionRegistry()
-
+    _flow: Flow = OpenAIFlow
+    _router: APIRouter = APIRouter
+    _function_registry: FunctionRegistry = FunctionRegistry
+        
     def __init__(self, *args, **kwargs):
         super().__init__(**kwargs)
-        self._router.prefix = self.name
-        self._function_registry.attach(functions=self.functions)
-
-    def __call__(self, path: str = "/", *args, **kwargs):
-        return self._function_registry.routes
-
+        self._router = APIRouter()
+        self._function_registry = FunctionRegistry()
+        self._router.prefix = f'/{self.name}'
+        self._router.get('/')(self.call)
+        self._function_registry.attach(functions = self.functions)
+        
+    def __call__(self, path: str = '/', *args, **kwargs):
+        return(self._function_registry.routes)
+        
     @property
     def router(self):
         self._router.include_router(self._function_registry)
@@ -49,17 +41,26 @@ class Agent(
     @property
     def app(self):
         self._app.include_router(self.router)
-        return self._app
-
+        return(self._app)
+    
+    async def call(self, q: str) -> str:
+        response = await self._flow(
+            engine = self.engine, 
+            functions = [route.endpoint for route in self._function_registry.routes]
+        ).start(q)
+        return response[-1].content
+    
     def register(self, *args, **kwargs):
         def decorator(func):
             def wrapper(*tool):
                 return func(*tool)
 
-            self._function_registry.attach(functions=[func], **kwargs)
-            self.functions = [
-                route.endpoint for route in self._function_registry.routes
-            ]
+            existing_function_names = [route.endpoint.__name__ for route in self._function_registry.routes]
+            if func.__name__ not in existing_function_names:
+                self._function_registry.attach(functions = [func], **kwargs)
+                self.functions = [route.endpoint for route in self._function_registry.routes]
+            
             return wrapper
 
         return decorator
+    
