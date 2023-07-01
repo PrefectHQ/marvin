7ceb269af5fda494a0de2059384a2c92b414f017
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2023-07-01 17:50:54 -0400

    Add docs writer

--- src/marvin/tools/filesystem.py
+++ src/marvin/tools/filesystem.py
@@ -78,24 +78,27 @@ class ReadFile(FileSystemTool):
         with open(path, "r") as f:
             content = f.readlines()
 
+        if start_row == 0:
+            start_row = 1
         if start_row > 0:
             start_row -= 1
         if end_row < 0:
             end_row += 1
 
         if end_row == 0:
-            content = content[start_row - 1 :]
+            content = content[start_row:]
         else:
-            content = content[start_row - 1 : end_row]
+            content = content[start_row:end_row]
 
         return "\n".join(content)
 
 
 class ReadFiles(FileSystemTool):
     description: str = """
-    Read the entire content of multiple files at once.{% if root_dir %} Paths
-    must be relative to {{ root_dir }}. Provide '.' instead of '/' to read
-    root.{%- endif %}}
+    Read the entire content of multiple files at once. Due to context size
+    limitations, reading too many files at once may cause truncated responses.
+    {% if root_dir %} Paths must be relative to {{ root_dir }}. Provide '.'
+    instead of '/' to read root.{%- endif %}}
     """
 
     def run(self, paths: list[str]) -> dict[str, str]:
@@ -117,9 +120,44 @@ class WriteContent(BaseModel):
     def check_insert_model(cls, values):
         if values["insert_at_row"] is None and values["write_mode"] == "insert":
             raise ValueError("Must provide `insert_at_row` when using `insert` mode.")
+        return values
 
 
-class WriteFiles(FileSystemTool):
+class WriteFile(FileSystemTool):
+    description: str = """
+        Write content to a file.
+        
+        {%if root_dir %} Paths must be relative to {{ root_dir }}.{% endif %}}
+        
+        {%if require_confirmation %} You MUST ask the user to confirm writes by
+        showing them details. {% endif %}
+        """
+    require_confirmation: bool = True
+
+    def run(self, write_content: WriteContent) -> str:
+        [path] = self.validate_paths([write_content.path])
+
+        # ensure the parent directory exists
+        path.parent.mkdir(parents=True, exist_ok=True)
+
+        if write_content.write_mode == "overwrite":
+            with open(path, "w") as f:
+                f.write(write_content.content)
+        elif write_content.write_mode == "append":
+            with open(path, "a") as f:
+                f.write(write_content.content)
+        elif write_content.write_mode == "insert":
+            with open(path, "r") as f:
+                contents = f.readlines()
+            contents[write_content.insert_at_row] = write_content.content
+
+            with open(path, "w") as f:
+                f.writelines(contents)
+
+        return f"Files {write_content.path} written successfully."
+
+
+class WriteFiles(WriteFile):
     description: str = """
         Write content to multiple files. Each `WriteContent` object in the
         `contents` argument is an instruction to write to a specific file.
@@ -134,23 +172,5 @@ class WriteFiles(FileSystemTool):
     @validate_arguments
     def run(self, contents: list[WriteContent]) -> str:
         for wc in contents:
-            [path] = self.validate_paths([wc.path])
-
-            # ensure the parent directory exists
-            path.parent.mkdir(parents=True, exist_ok=True)
-
-            if wc.write_mode == "overwrite":
-                with open(path, "w") as f:
-                    f.write(wc.content)
-            elif wc.write_mode == "append":
-                with open(path, "a") as f:
-                    f.write(wc.content)
-            elif wc.write_mode == "insert":
-                with open(path, "r") as f:
-                    contents = f.readlines()
-                contents[wc.insert_at_row] = wc.content
-
-                with open(path, "w") as f:
-                    f.writelines(contents)
-
+            super().run(write_content=wc)
         return f"Files {[c.path for c in contents]} written successfully."
