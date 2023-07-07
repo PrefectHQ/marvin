f486bb6d19e9d3a6ca290906bd450ff77687e97a
Nathan Nowack <thrast36@gmail.com>
2023-07-06 22:14:21 -0500

    add plan patch tests

--- tests/test_primitives/test_ai_app.py
+++ tests/test_primitives/test_ai_app.py
@@ -2,7 +2,10 @@ import jsonpatch
 import pytest
 from marvin.components.ai_application.base import (
     AIApplication,
+    AppPlan,
     FreeformState,
+    TaskState,
+    UpdatePlan,
     UpdateState,
 )
 
@@ -33,3 +36,77 @@ class TestJSONPatch:
         with pytest.raises(jsonpatch.JsonPatchConflict):
             tool.run([{"op": "replace", "path": "/state/baz", "value": "qux"}])
         assert app.state.dict() == {"state": {"foo": "bar"}}
+
+
+class TestPlanJSONPatch:
+    def test_update_app_plan_valid_patch(self):
+        app = AIApplication(
+            plan=AppPlan(
+                tasks=[{"id": 1, "description": "test task", "state": "IN_PROGRESS"}]
+            ),
+            description="test app",
+        )
+        tool = UpdatePlan(app=app)
+        tool.run([{"op": "replace", "path": "/tasks/0/state", "value": "COMPLETED"}])
+        assert app.plan.dict() == {
+            "tasks": [
+                {
+                    "id": 1,
+                    "description": "test task",
+                    "state": TaskState.COMPLETED,
+                    "upstream_task_ids": None,
+                    "parent_task_id": None,
+                }
+            ],
+            "notes": [],
+        }
+
+    def test_update_app_plan_invalid_patch(self):
+        app = AIApplication(
+            plan=AppPlan(
+                tasks=[{"id": 1, "description": "test task", "state": "IN_PROGRESS"}]
+            ),
+            description="test app",
+        )
+        tool = UpdatePlan(app=app)
+        with pytest.raises(jsonpatch.JsonPatchException):
+            tool.run(
+                [{"op": "invalid_op", "path": "/tasks/0/state", "value": "COMPLETED"}]
+            )
+        assert app.plan.dict() == {
+            "tasks": [
+                {
+                    "id": 1,
+                    "description": "test task",
+                    "state": TaskState.IN_PROGRESS,
+                    "upstream_task_ids": None,
+                    "parent_task_id": None,
+                }
+            ],
+            "notes": [],
+        }
+
+    def test_update_app_plan_non_existent_path(self):
+        app = AIApplication(
+            plan=AppPlan(
+                tasks=[{"id": 1, "description": "test task", "state": "IN_PROGRESS"}]
+            ),
+            description="test app",
+        )
+        tool = UpdatePlan(app=app)
+        with pytest.raises(jsonpatch.JsonPointerException):
+            tool.run(
+                [{"op": "replace", "path": "/tasks/1/state", "value": "COMPLETED"}]
+            )
+        assert app.plan.dict() == {
+            "tasks": [
+                {
+                    "id": 1,
+                    "description": "test task",
+                    "state": TaskState.IN_PROGRESS,
+                    "upstream_task_ids": None,
+                    "parent_task_id": None,
+                }
+            ],
+            "notes": [],
+        }
