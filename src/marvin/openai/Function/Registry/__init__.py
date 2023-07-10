db0f985ff1de07ac234e83b42424d096dd9e006d
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2023-07-09 21:35:54 -0400

    refactor

--- src/marvin/openai/Function/Registry/__init__.py
+++ src/marvin/openai/Function/Registry/__init__.py
@@ -5,53 +5,7 @@ from marvin.utilities.types import function_to_model
 from marvin.models.messages import Message
 from marvin.openai.Function import marvin_fn, openai_fn
 from openai.openai_object import OpenAIObject
-
-
-class FunctionRegistry(APIRouter):
-    def __init__(self, function_decorator=marvin_fn, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.function_decorator = function_decorator
-
-    @property
-    def endpoints(self):
-        return [route.endpoint for route in self.routes]
-
-    @property
-    def schema(self):
-        return [fn.schema() for fn in self.endpoints]
-
-    @property
-    def functions(self):
-        return [fn.schema() for fn in self.endpoints]
-
-    def include(self, registry: "FunctionRegistry", *args, **kwargs):
-        super().include_router(registry, *args, **kwargs)
-        # Add some 50-IQ idempotency.
-        self.routes = list({x.name: x for x in self.routes}.values())
-
-    def register(self, fn: Optional[Callable] = None, **kwargs: Any) -> Callable:
-        def decorator(fn: Callable, *args) -> Callable:
-            fn = self.function_decorator(fn=fn, **kwargs)
-            self.add_api_route(
-                **{
-                    **{
-                        "name": fn.name,
-                        "path": f"/{fn.name}",
-                        "endpoint": fn,
-                        "description": fn.description,
-                        "methods": ["POST"],
-                    },
-                    **kwargs,
-                }
-            )
-            return fn
-
-        if fn:
-            # if the decorator was called with parentheses
-            return decorator(fn)
-        else:
-            # else, return the decorator to be called later
-            return decorator
+from marvin.functions import FunctionRegistry
 
 
 class OpenAIFunctionRegistry(FunctionRegistry):
--- src/marvin/openai/Function/__init__.py
+++ src/marvin/openai/Function/__init__.py
@@ -4,33 +4,11 @@ from openai.openai_object import OpenAIObject
 import functools
 import json
 from pydantic import validate_arguments
+from marvin.functions import Function, FunctionDecoratorFactory
 
 
 T = TypeVar("T")
 A = TypeVar("A")
-
-
-class Function:
-    def __init__(
-        self, *, fn: Callable[[A], T] = None, name: str = None, description: str = None
-    ) -> None:
-        self.fn = fn
-        self.name = name or self.fn.__name__
-        self.description = description or self.fn.__doc__
-
-        super().__init__()
-
-    @property
-    def model(self):
-        return function_to_model(self.fn, name=self.name, description=self.description)
-
-    def schema(self, *args, name: str = None, description: str = None, **kwargs):
-        schema = self.model.schema(*args, **kwargs)
-        return {
-            "name": name or schema.pop("title"),
-            "description": description or self.fn.__doc__,
-            "parameters": schema,
-        }
 
 
 class OpenAIFunction(Function):
@@ -61,23 +39,4 @@ class OpenAIFunction(Function):
             return responses
 
 
-def FunctionDecoratorFactory(
-    name: str = "marvin", func_class: Type[T] = None
-) -> Callable[[A], T]:
-    def decorator(fn: Callable[[A], T] = None) -> Callable[[A], T]:
-        if fn is None:
-            return functools.partial(decorator)
-        else:
-            instance = func_class(fn=fn)
-            setattr(fn, name, instance)
-            for method in dir(instance):
-                is_method_private = method.startswith("__")
-                if not is_method_private:
-                    setattr(fn, method, getattr(instance, method))
-        return fn
-
-    return decorator
-
-
-marvin_fn = FunctionDecoratorFactory(name="marvin", func_class=Function)
 openai_fn = FunctionDecoratorFactory(name="openai", func_class=OpenAIFunction)
