3df2fb40299aa25542ad14f481a51cfd320f53f8
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2023-07-10 14:22:55 -0400

    Update docs writer

--- cookbook/docs_writer.py
+++ cookbook/docs_writer.py
@@ -25,18 +25,29 @@ docs_app = AIApplication(
         documentation is based on Material for MKDocs and has a mix of
         conceptual guides, tutorials, and reference documentation. You can
         update and create new documentation as necessary, and be sure to update
-        the `mkdocs.yml` file's navigation section appropriately. Emulate the
-        style of the existing documentation as much as possible.
+        the `mkdocs.yml` file's navigation section appropriately. 
+        
+        Style and tone: Marvin's documentation is written in a somewhat
+        informal, conversational tone, but remains professional and clear. At
+        all times it is an expert teacher. It uses a mix of technical language
+        (e.g. "observability", "tracing", "autonomous agents") and occiasionally
+        drops into more casual, relatable language for certain calls-to-action
+        or taglines (e.g. "You know Python? You know Marvin."). Above all,
+        Marvin's documention seeks to accelerate users and minimize
+        time-to-value as much as possible by being direct and to the point,
+        prioritizing brevity and clarity over elaboration. This does not mean it
+        avoids details or longer explanations, as those can be important or even
+        necessary for some users, but users shouldn't have to wade any deeper
+        than necessary to get the information they need.  This reflects a
+        pragmatic, no-nonsense attitude, appealing to practical-minded
+        developers who want clear and concise information. However, humor and
+        levity come naturally through the framework's cultural homage to The
+        Hitchhiker's Guide to the Galaxy and can be used sparingly to add
+        personality.
         
         The library source is available at {ROOT_DIR}. The documentation is
         contained in {DOCS_DIR}. The `mkdocs.yml` file is located at
         {MKDOCS_FILE}.
-        
-        You should maintain an overview of all documentation at
-        docs/_ai_overview.md. This file is only for use by AIs like you, so
-        design it to be as helpful as possible (ideally it should allow you to
-        quickly identify relevant documentation without reading everything). Be
-        sure to update this file as you modify the documentation.
         """,
     tools=[
         marvin.tools.filesystem.ListFiles(root_dir=ROOT_DIR),
