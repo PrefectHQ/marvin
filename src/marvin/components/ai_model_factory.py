e1880883149f3525dc2f219ae96d1466ce8dcf73
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2023-07-17 20:33:34 -0400

    Move meta schema

--- src/marvin/components/ai_model_factory.py
+++ src/marvin/components/ai_model_factory.py
@@ -1,9 +1,19 @@
-from typing import TypeVar
+from typing import Optional
+
+from pydantic import BaseModel
 
 from marvin.components.ai_model import ai_model
-from marvin.models.meta import DataSchema
 
-T = TypeVar("T")
+
+class DataSchema(BaseModel):
+    title: Optional[str] = None
+    type: Optional[str] = None
+    properties: Optional[dict] = {}
+    required: Optional[list[str]] = []
+    additionalProperties: bool = False
+    definitions: dict = {}
+    description: Optional[str] = None
+
 
 # If you're reading this and expected something fancier,
 # I'm sorry to disappoint you. It's this simple.
