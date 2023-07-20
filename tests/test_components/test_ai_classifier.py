98e756fdaf54bcf3653cae1e3c43d875120d9f30
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2023-07-20 08:15:00 -0400

    Add tests

--- tests/test_components/test_ai_classifier.py
+++ tests/test_components/test_ai_classifier.py
@@ -4,8 +4,18 @@ import pytest
 from marvin import ai_classifier
 from marvin.engine.language_models import chat_llm
 
+from tests.utils.mark import pytest_mark_class
 
-class TestAIClassifiers:
+
+class TestAIClassifiersInitialization:
+    def test_model(self):
+        @ai_classifier(model=chat_llm("openai/gpt-4-test-model"))
+        class Sentiment(Enum):
+            POSITIVE = "Positive"
+            NEGATIVE = "Negative"
+
+        assert Sentiment.__model__.model == "gpt-4-test-model"
+
     def test_invalid_model(self):
         @ai_classifier(model=chat_llm("anthropic/claude-2"))
         class Sentiment(Enum):
@@ -23,3 +33,50 @@ class TestAIClassifiers:
 
         with pytest.raises(ValueError, match="(max_tokens=1)"):
             Sentiment("Great!")
+
+
+@pytest_mark_class("llm")
+class TestAIClassifiers:
+    def test_sentiment(self):
+        @ai_classifier
+        class Sentiment(Enum):
+            POSITIVE = "Positive"
+            NEGATIVE = "Negative"
+
+        assert Sentiment("Great!") == Sentiment.POSITIVE
+
+    def test_keys_are_passed_to_llm(self):
+        @ai_classifier
+        class Sentiment(Enum):
+            POSITIVE = "option - 1"
+            NEGATIVE = "option - 2"
+
+        assert Sentiment("Great!") == Sentiment.POSITIVE
+
+    def test_values_are_passed_to_llm(self):
+        @ai_classifier
+        class Sentiment(Enum):
+            OPTION_1 = "Positive"
+            OPITION_2 = "Negative"
+
+        assert Sentiment("Great!") == Sentiment.OPTION_1
+
+    def test_docstring_is_passed_to_llm(self):
+        @ai_classifier
+        class Sentiment(Enum):
+            """It's opposite day"""
+
+            POSITIVE = "Positive"
+            NEGATIVE = "Negative"
+
+        assert Sentiment("Great!") == Sentiment.NEGATIVE
+
+    def test_instructions_are_passed_to_llm(self):
+        @ai_classifier
+        class Sentiment(Enum):
+            POSITIVE = "Positive"
+            NEGATIVE = "Negative"
+
+        assert (
+            Sentiment("Great!", instructions="It's opposite day") == Sentiment.NEGATIVE
+        )
