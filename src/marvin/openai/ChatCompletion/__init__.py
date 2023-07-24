9213101fab6984c97ddeb2ed36a7032b3a89e8c2
Nathan Nowack <thrast36@gmail.com>
2023-07-24 08:30:54 -0500

    tweak merge and default api key to marvin setting

--- src/marvin/openai/ChatCompletion/__init__.py
+++ src/marvin/openai/ChatCompletion/__init__.py
@@ -1,7 +1,7 @@
 import functools
 
 import openai
-from pydantic import BaseSettings, Field, SecretStr
+from pydantic import BaseSettings, Field, validator
 
 import marvin
 
@@ -9,29 +9,28 @@ import marvin
 class ChatCompletionConfig(BaseSettings):
     model: str = "gpt-3.5-turbo"
     temperature: float = 0
-    functions: list = []
-    messages: list = []
+    functions: list = Field(default_factory=list)
+    messages: list = Field(default_factory=list)
     api_key: str = Field(
         default_factory=lambda: (
             marvin.settings.openai.api_key.get_secret_value()
-            if isinstance(marvin.settings.openai.api_key, SecretStr)
-            else ""
-        )
+            if marvin.settings.openai.api_key is not None
+            else None
+        ),
+        env="OPENAI_API_KEY",
     )
 
     def merge(self, *args, **kwargs):
-        _dict = self.dict(exclude_unset=True)
         for key, value in kwargs.items():
             if type(value) == list:
-                _dict[key] = _dict.get(key, []) + value
+                setattr(self, key, getattr(self, key, []) + value)
             else:
-                _dict[key] = value
-        return _dict
+                setattr(self, key, value)
+        return {k: v for k, v in self.__dict__.items() if v != []}
 
 
 class ChatCompletion(openai.ChatCompletion):
     def __new__(cls, *args, **kwargs):
-        print(ChatCompletionConfig(**kwargs))
         subclass = type(
             "ChatCompletion",
             (ChatCompletion,),
