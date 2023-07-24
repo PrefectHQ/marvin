639558921700008b69b05df2622cd2c0a68337e2
Nathan Nowack <thrast36@gmail.com>
2023-07-24 06:05:43 -0500

    pass api key from settings

--- src/marvin/openai/ChatCompletion/__init__.py
+++ src/marvin/openai/ChatCompletion/__init__.py
@@ -1,14 +1,20 @@
-import openai
-import pydantic
 import functools
 
+import openai
+from pydantic import BaseSettings, Field
 
-class ChatCompletionConfig(pydantic.BaseSettings):
+import marvin
+
+
+class ChatCompletionConfig(BaseSettings):
     model: str = "gpt-3.5-turbo"
     temperature: float = 0
     functions: list = []
     messages: list = []
-    api_key: str = pydantic.Field(default="", env="OPENAI_API_KEY")
+    api_key: str = Field(
+        default_factory=lambda: marvin.settings.openai.api_key.get_secret_value() or "",
+        env="OPENAI_API_KEY",
+    )
 
     def merge(self, *args, **kwargs):
         _dict = self.dict(exclude_unset=True)
