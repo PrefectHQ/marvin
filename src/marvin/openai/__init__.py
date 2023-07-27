cbe83a8d65ab95c42051ae676d1fa5913408edba
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2023-07-26 23:24:17 -0400

    Update __init__.py

--- src/marvin/openai/__init__.py
+++ src/marvin/openai/__init__.py
@@ -1,10 +1,13 @@
+import sys
+import openai
 from .ChatCompletion import ChatCompletion, Request
 from .Function import openai_fn
 from .Function.Registry import OpenAIFunctionRegistry
 
-__all__ = [
-    "ChatCompletion",
-    "Request",
-    "openai_fn",
-    "OpenAIFunctionRegistry",
-]
+openai.ChatCompletion = ChatCompletion
+openai.openai_fn = openai_fn
+openai.OpenAIFunctionRegistry = OpenAIFunctionRegistry
+
+
+# TODO: This is a hack to make the openai module available in the global namespace
+sys.modules[__name__] = openai
