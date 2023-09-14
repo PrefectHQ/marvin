df7fac302ed77287527ebbd6daea4c5732d5b4e4
Nathan Nowack <thrast36@gmail.com>
2023-09-13 23:48:07 -0500

    more info

--- src/marvin/cli/__init__.py
+++ src/marvin/cli/__init__.py
@@ -1,6 +1,5 @@
 from .typer import AsyncTyper
 
-
 from .admin import app as admin
 from .chat import chat
 
@@ -13,9 +12,15 @@ app.acommand()(chat)
 
 @app.command()
 def version():
+    import platform
+    import sys
     from marvin import __version__
 
-    print(__version__)
+    print(f"Version:\t\t{__version__}")
+
+    print(f"Python version:\t\t{sys.version.split()[0]}")
+
+    print(f"OS/Arch:\t\t{platform.system().lower()}/{platform.machine().lower()}")
 
 
 if __name__ == "__main__":
