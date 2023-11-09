46932b93a83866e592d8dfdca6aaa7cbe62e8f53
Nathan Nowack <thrast36@gmail.com>
2023-11-08 21:39:57 -0600

    add whimsical CLI tool

--- src/marvin/cli/__init__.py
+++ src/marvin/cli/__init__.py
@@ -1,19 +1,53 @@
-from .typer import AsyncTyper
+import sys
+import typer
+from rich.console import Console
+from typing import Optional
+from marvin.utilities.asyncutils import run_sync
+from marvin.utilities.openai import get_client
+from marvin.cli.version import display_version
 
-app = AsyncTyper()
+app = typer.Typer()
+console = Console()
+
+app.command(name="version")(display_version)
 
 
-@app.command()
-def version():
-    import platform
-    import sys
-    from marvin import __version__
+@app.callback(invoke_without_command=True)
+def main(
+    ctx: typer.Context,
+    model: Optional[str] = typer.Option("gpt-3.5-turbo"),
+    max_tokens: Optional[int] = typer.Option(1000),
+):
+    if ctx.invoked_subcommand is not None:
+        return
+    elif ctx.invoked_subcommand is None and not sys.stdin.isatty():
+        run_sync(process_stdin(model, max_tokens))
+    else:
+        console.print(ctx.get_help())
 
-    print(f"Version:\t\t{__version__}")
 
-    print(f"Python version:\t\t{sys.version.split()[0]}")
+async def process_stdin(model: str, max_tokens: int):
+    client = get_client()
+    content = sys.stdin.read()
+    last_chunk_ended_with_space = False
 
-    print(f"OS/Arch:\t\t{platform.system().lower()}/{platform.machine().lower()}")
+    async for part in await client.chat.completions.create(
+        model=model,
+        messages=[{"role": "user", "content": content}],
+        max_tokens=max_tokens,
+        stream=True,
+    ):
+        print_chunk(part, last_chunk_ended_with_space)
+
+
+def print_chunk(part, last_chunk_flag):
+    text_chunk = part.choices[0].delta.content or ""
+    if text_chunk:
+        if last_chunk_flag and text_chunk.startswith(" "):
+            text_chunk = text_chunk[1:]
+        sys.stdout.write(text_chunk)
+        sys.stdout.flush()
+        last_chunk_flag = text_chunk.endswith(" ")
 
 
 if __name__ == "__main__":
