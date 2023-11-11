4d44f6024b9ee64970bb5b75d3af0b76e80dfd0b
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2023-11-10 21:12:03 -0500

    Memoize client

--- src/marvin/utilities/openai.py
+++ src/marvin/utilities/openai.py
@@ -1,3 +1,4 @@
+from functools import cache
 from typing import Optional
 
 from openai import AsyncClient
@@ -10,7 +11,18 @@ def get_client() -> AsyncClient:
         settings.openai.api_key.get_secret_value() if settings.openai.api_key else None
     )
     organization: Optional[str] = settings.openai.organization
+    return _get_client_memoized(api_key=api_key, organization=organization)
 
+
+@cache
+def _get_client_memoized(
+    api_key: Optional[str],
+    organization: Optional[str],
+) -> AsyncClient:
+    """
+    This function is memoized to ensure that only one instance of the client is
+    created for a given api key / organization pair.
+    """
     return AsyncClient(
         api_key=api_key,
         organization=organization,
