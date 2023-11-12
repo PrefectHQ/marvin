f05a507e2daaa4d33f165a3e1cd1dcfd8509d6f5
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2023-11-12 14:28:54 -0500

    FIX: ensure openai clients are not shared across loops

--- src/marvin/utilities/openai.py
+++ src/marvin/utilities/openai.py
@@ -1,4 +1,5 @@
-from functools import cache
+import asyncio
+from functools import lru_cache
 from typing import Optional
 
 from openai import AsyncClient
@@ -11,17 +12,25 @@ def get_client() -> AsyncClient:
         settings.openai.api_key.get_secret_value() if settings.openai.api_key else None
     )
     organization: Optional[str] = settings.openai.organization
-    return _get_client_memoized(api_key=api_key, organization=organization)
+    return _get_client_memoized(
+        api_key=api_key, organization=organization, loop=asyncio.get_event_loop()
+    )
 
 
-@cache
+@lru_cache
 def _get_client_memoized(
     api_key: Optional[str],
     organization: Optional[str],
+    loop: Optional[asyncio.AbstractEventLoop] = None,
 ) -> AsyncClient:
     """
     This function is memoized to ensure that only one instance of the client is
-    created for a given api key / organization pair.
+    created for a given api key / organization / loop tuple.
+
+    The `loop` is an important key to ensure that the client is not re-used
+    across multiple event loops (which can happen when using the `run_sync`
+    function). Attempting to re-use the client across multiple event loops
+    can result in a `RuntimeError: Event loop is closed` error or infinite hangs.
     """
     return AsyncClient(
         api_key=api_key,
