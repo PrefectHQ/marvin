001894c2241b035a15b9b04b54fca3d1482fa835
Nathan Nowack <thrast36@gmail.com>
2023-11-17 10:46:21 -0600

    clarifying comment

--- tests/utils.py
+++ tests/utils.py
@@ -10,10 +10,12 @@ def pytest_mark_class(*markers: str):
 
     def mark_test_methods(cls):
         for attr_name, attr_value in cls.__dict__.items():
+            # mark all test methods with the provided markers
             if callable(attr_value) and attr_name.startswith("test"):
                 for marker in markers:
                     marked_func = getattr(pytest.mark, marker)(attr_value)
                     setattr(cls, attr_name, marked_func)
+            # recursively mark nested classes
             elif isinstance(attr_value, type) and attr_value.__name__.startswith(
                 "Test"
             ):
