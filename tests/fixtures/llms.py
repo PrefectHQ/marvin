033aa8464e87a24e794f41b28e5ab944d5429c97
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2024-01-07 14:54:45 -0500

    Fix env var reference for gpt4

--- tests/fixtures/llms.py
+++ tests/fixtures/llms.py
@@ -7,5 +7,5 @@ def gpt_4():
     """
     Uses GPT 4 for the duration of the test
     """
-    with temporary_settings(openai__llms__model="gpt-4-1106-preview"):
+    with temporary_settings(openai__chat__completions__model="gpt-4-1106-preview"):
         yield
