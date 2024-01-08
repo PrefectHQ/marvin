54fe7b2c517cd58b799348e63ecc04afad522130
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2024-01-07 22:07:15 -0500

    Add LLM unit tests

--- tests/components/v2/test_generate.py
+++ tests/components/v2/test_generate.py
@@ -1,4 +1,5 @@
 import marvin.v2
+from marvin.utilities.testing import assert_equal
 from pydantic import BaseModel, Field
 
 from tests.utils import pytest_mark_class
@@ -10,10 +11,20 @@ class Location(BaseModel):
 
 
 @pytest_mark_class("llm")
-class TestCast:
+class TestGenerate:
     class TestBuiltins:
         def test_toy_generate(self):
+            result = marvin.v2.generate(int, n=3)
+            assert_equal(result, "a list of three integers")
+
+        def test_generate_locations(self):
+            result = marvin.v2.generate(Location, n=3)
+            assert_equal(result, "a list of three locations")
+
+        def test_generate_locations_with_instructions(self):
             result = marvin.v2.generate(
-                int, n=3, instructions="always generate the first 3 integers"
+                Location, n=3, instructions="major cities in California"
             )
-            assert result == [1, 2, 3]
+            assert_equal(
+                result, "a list of three locations that are major cities in California"
+            )
