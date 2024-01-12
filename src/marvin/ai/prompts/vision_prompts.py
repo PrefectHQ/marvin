5f7db6e1ea6a655625aad7b25d716deb1af0925f
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2024-01-12 10:30:10 -0500

    Update src/marvin/ai/prompts/vision_prompts.py
    Co-authored-by: nate nowack <thrast36@gmail.com>

--- src/marvin/ai/prompts/vision_prompts.py
+++ src/marvin/ai/prompts/vision_prompts.py
@@ -2,7 +2,7 @@ import inspect
 
 CAPTION_PROMPT = inspect.cleandoc(
     """
-    Generate a descriptive caption the following image, and pay attention to any
+    Generate a descriptive caption for the following image, and pay attention to any
     additional instructions. Do not respond directly to the user ("you"), as
     your response will become the input for other text processing functions.
 
