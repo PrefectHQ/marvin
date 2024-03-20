0fbabf7bfdbb0245c65ea29fafd2239811acf924
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2024-03-19 21:57:37 -0400

    Remove commented code

--- src/marvin/beta/assistants/handlers.py
+++ src/marvin/beta/assistants/handlers.py
@@ -7,63 +7,6 @@ from typing_extensions import override
 
 from marvin.beta.assistants.formatting import format_run
 
-# class MarvinHandler(AsyncAssistantEventHandler):
-#     """
-#     A container that composes multiple AssistantEventHandlers or
-#     AsyncAssistantEventHandlers into a single object by delegating the on_*
-#     methods to each handler.
-#     """
-
-#     def __init__(self, handlers: list[AssistantEventHandler] = None):
-#         self.handlers = handlers or []
-#         super().__init__()
-
-#     # def __setattr__(self, name, value):
-#     #     # Set the attribute on the RunHandler instance
-#     #     super().__setattr__(name, value)
-
-#     #     # Also set the attribute on each handler
-#     #     for handler in self.handlers:
-#     #         setattr(handler, name, value)
-
-#     def __getattribute__(self, name):
-#         parent_attr = super().__getattribute__(name)
-
-#         # Check if the method starts with 'on_'
-#         if name.startswith("on_")
-
-#             # if it does, return a special method that calls the equivalent method
-#             # on each handler (accounting for async methods)
-#             async def _on_event_method(*args, **kwargs):
-#                 await parent_attr(*args, **kwargs)
-#                 for handler in self.handlers:
-#                     method = getattr(handler, name)
-#                     result = method(*args, **kwargs)
-#                     if inspect.iscoroutine(result):
-#                         await result
-
-#             return _on_event_method
-
-#         else:
-#             # Otherwise, access the RunHandler's true attribute
-#             return parent_attr
-
-#     async def get_final_messages(self):
-#         # this errors if no messages were generated, which can happen
-#         # when making tool calls
-#         try:
-#             return await super().get_final_messages()
-#         except RuntimeError:
-#             return []
-
-#     async def get_final_run_steps(self):
-#         # this errors if no run_steps were generated, which can happen
-#         # when making tool calls
-#         try:
-#             return await super().get_final_run_steps()
-#         except RuntimeError:
-#             return []
-
 
 class PrintHandler(AsyncAssistantEventHandler):
     def __init__(self, print_messages: bool = True, print_steps: bool = True):
