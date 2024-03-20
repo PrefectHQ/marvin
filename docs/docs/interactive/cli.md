9e0e0fa9c0bc3da50fe2f8c461afc7033093daf9
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2024-03-20 18:55:07 -0400

    Add support for custom assistants

--- docs/docs/interactive/cli.md
+++ docs/docs/interactive/cli.md
@@ -1,9 +1,8 @@
 # CLI
 
-Marvin includes a CLI for quickly invoking an AI assistant. 
+Marvin includes a CLI for quickly invoking an AI assistant.
 
 ![](/assets/images/docs/cli/hero.png)
-
 
 ## Tools
 
@@ -16,6 +15,7 @@ By default, the CLI assistant has the following tools:
 To learn more about tools, see the [tools documentation](/docs/interactive/assistants/#tools).
 
 ![](/assets/images/docs/cli/tools.png)
+
 ## Threads
 
 The CLI assistant automatically remembers the history of your conversation. You can use threads to create multiple simultaneous conversations. To learn more about threads, see the [threads documentation](/docs/interactive/assistants/#threads).
@@ -23,6 +23,7 @@ The CLI assistant automatically remembers the history of your conversation. You 
 ### Changing threads
 
 By default, the CLI assistant uses a global default thread. For example, this posts two messages to the global default thread:
+
 ```bash
 marvin say "Hello!"
 marvin say "How are you?"
@@ -31,12 +32,14 @@ marvin say "How are you?"
 To change the thread on a per-message basis, use the `--thread` or `-t` flag and provide a thread name. Thread names are arbitrary and can be any string; it's a way for you to group conversations together.
 
 This posts one message to a thread called "my-thread" and another to a thread called "my-other-thread":
+
 ```bash
 marvin say "Hello!" -t my-thread
 marvin say "How are you?" -t my-other-thread
 ```
 
 To change the default thread for multiple messages, use the `MARVIN_CLI_THREAD` environment variable. You can do this globally or `export` it in your shell for a single session. For example, this sets the default thread to "my-thread":
+
 ```bash
 export MARVIN_CLI_THREAD=my-thread
 
@@ -47,11 +50,13 @@ marvin say "How are you?"
 ### Resetting threads
 
 To reset a thread, use the `reset` command. This will give you a completely fresh history and context for the thread. For example, this resets the default thread:
+
 ```bash
 marvin thread reset
 ```
 
 And this resets a thread called "my-thread":
+
 ```bash
 marvin thread reset -t my-thread
 ```
@@ -59,11 +64,73 @@ marvin thread reset -t my-thread
 ### Current thread
 
 To see the current thread (and corresponding OpenAI thread ID), use the `current` command. For example:
+
 ```bash
 marvin thread current
 ```
 
+## Custom assistants
+
+The Marvin CLI allows you to register and use custom assistants in addition to the default assistant. Custom assistants are defined in Python files and can have their own set of instructions, tools, and behaviors.
+
+### Registering an assistant
+
+To register a custom assistant, create a Python file that defines an instance of the `Assistant` class from the `marvin.beta.assistants` module. To learn more about creating assistants and available options, see the [assistants documentation](/docs/interactive/assistants/).
+
+Next, use the Marvin CLI to register the assistant by providing the fully-qualified path to the Python file and the variable that contains the assistant. For example, if you saved a file `custom_assistant.py` with the following contents:
+
+```python
+# path/to/custom_assistant.py
+
+from marvin.beta.assistants import Assistant, CodeInterpreter
+
+my_assistant = Assistant(
+    name="Arthur",
+    instructions="A parody of Arthur Dent",
+    tools=[CodeInterpreter]
+)
+```
+
+Then you would register this assistant with the following command:
+
+```bash
+marvin assistant register path/to/custom_assistant.py:my_assistant
+```
+
+This will automatically use the assistant's name (Arthur) as the name of the assistant in the Marvin CLI registry. Each registered assistant must have a unique name, so you can override the name during registration with the `--name` or `-n` flag. For example:
+
+```bash
+marvin assistant register path/to/custom_assistant.py:my_assistant -n "My Custom Assistant"
+```
+
+By default, registering an assistant with the same name as an existing one will fail. To overwrite an existing assistant, use the --overwrite or -o flag.
+
+!!! warning 
+    When you register an assistant, its name and the path to the file are stored in the Marvin registry. This allows the CLI to load the assistant whenever you need it. However, it means the assistant file must remain in the same location, with the same name, for the CLI to find it. If you move or rename the file, you will need to re-register the assistant. However, if you edit the file without changing the variable name of the assistant, the CLI will automatically use the updated assistant.
+
+    During registration, you can optionally copy the assistant file to the Marvin assistant registry by passing the `--copy` or `-c` flag. This will create a copy of the file in the registry, but note that any subsequent changes to the original file will not be reflected in the registered assistant unless you re-register it.
 
 
+### Using an assistant
 
+To use a custom assistant when sending a message, use the `--assistant` or `-a` flag followed by the name of the registered assistant. For example, if you registered an assistant named "Arthur", you can talk to it like this:
 
+```bash
+marvin say "Hello!" -a "Arthur"
+```
+
+You can also set a default assistant using the MARVIN_CLI_ASSISTANT environment variable, similar to setting a default thread. This allows you to set a global or session-specific default assistant.
+
+### Listing registered assistants
+
+To see a list of all registered assistants, use the `marvin assistant list` command. This will display a table with the names and file paths of the registered assistants.
+
+### Deleting a registered assistant
+
+To remove a registered assistant, use the `marvin assistant delete` command followed by the name of the assistant. For example:
+
+```bash
+marvin assistant delete "My Custom Assistant"
+```
+
+Note that this only removes the reference to the assistant in the Marvin registry and does not delete the actual assistant file, even if you used the `--copy` flag during registration.
--- src/marvin/cli/assistants.py
+++ src/marvin/cli/assistants.py
@@ -1,5 +1,12 @@
+import importlib
+from pathlib import Path
+from typing import Optional, Union
+
 import httpx
 import typer
+from pydantic import BaseModel, ValidationError
+from rich.console import Console
+from rich.table import Table
 
 from marvin.beta.assistants import Assistant, Thread
 from marvin.tools.assistants import CodeInterpreter
@@ -8,6 +15,69 @@ from marvin.tools.filesystem import getcwd, ls, read, read_lines
 from . import threads as threads_cli
 
 assistants_app = typer.Typer(no_args_is_help=True)
+
+ASSISTANTS_DIR = Path.home() / ".marvin/cli/assistants"
+
+console = Console()
+
+
+class AssistantData(BaseModel):
+    name: str
+    path: Path
+
+
+def get_assistant_file_path(name: str) -> Path:
+    return ASSISTANTS_DIR / f"{name}.json"
+
+
+def save_assistant(assistant_data: AssistantData):
+    assistant_file = get_assistant_file_path(assistant_data.name)
+    assistant_file.write_text(assistant_data.model_dump_json())
+
+
+def load_assistant_data(name: str) -> Optional[AssistantData]:
+    assistant_file = get_assistant_file_path(name)
+    if assistant_file.exists():
+        try:
+            return AssistantData.model_validate_json(assistant_file.read_text())
+        except ValidationError:
+            assistant_file.unlink()
+            return None
+    else:
+        return None
+
+
+def load_assistant(name: str) -> Optional[Assistant]:
+    assistant_data = load_assistant_data(name)
+
+    if not assistant_data:
+        raise ValueError(f"Assistant '{name}' not found")
+
+    return load_assistant_from_path(assistant_data.path)
+
+
+def load_assistant_from_path(path: Union[str, Path]) -> Assistant:
+    try:
+        module_path, assistant_name = str(path).split(":")
+    except ValueError:
+        raise ValueError("Path must be in the format 'path/to/module.py:AssistantName'")
+
+    if not Path(module_path).exists():
+        raise ValueError(f"Could not find file at path {module_path}")
+
+    module_spec = importlib.util.spec_from_file_location(
+        "custom_assistant", module_path
+    )
+    module = importlib.util.module_from_spec(module_spec)
+    module_spec.loader.exec_module(module)
+    assistant = getattr(module, assistant_name, None)
+    if not assistant:
+        raise ValueError("Could not load assistant 'in module")
+    elif not isinstance(assistant, Assistant):
+        raise TypeError(
+            "Assistant must be an instance of marvin.beta.assistants.Assistant"
+        )
+    return assistant
 
 
 def browse(url: str) -> str:
@@ -37,6 +107,94 @@ default_assistant = Assistant(
 )
 
 
+@assistants_app.command("register")
+def register_assistant(
+    path: Path = typer.Argument(
+        ...,
+        help="Path to the Python file containing the assistant object, in the form path/to/file.py:assistant_name",
+    ),
+    name: str = typer.Option(
+        None,
+        "--name",
+        "-n",
+        help="A name for the assistant, taken from the assistant if not provided. Must be unique.",
+    ),
+    overwrite: bool = typer.Option(
+        False,
+        "--overwrite",
+        "-o",
+        help="Overwrite the existing assistant, if it exists.",
+    ),
+    copy_file: bool = typer.Option(
+        False,
+        "--copy",
+        "-c",
+        help="Copy the file to the assistant registry. Note: edits to the original file will not be reflected in the assistant without re-registering.",
+    ),
+):
+    try:
+        assistant = load_assistant_from_path(path)
+    except Exception as exc:
+        typer.echo(exc)
+        raise typer.Exit(1)
+
+    name = name or assistant.name
+
+    # copy the file to the assistant directory
+    if copy_file:
+        new_path = ASSISTANTS_DIR / f"{name}.py"
+        # copy the file to the assistant directory
+        new_path.write_text(Path(str(path).split(":")[0]).read_text())
+        path = new_path
+
+    if not name:
+        typer.echo("No name provided and assistant has no name attribute.")
+        raise typer.Exit(1)
+    assistant_data = AssistantData(name=name, path=path)
+
+    if not overwrite:
+        existing_assistant = load_assistant_data(name)
+        if existing_assistant:
+            typer.echo(f"Assistant '{name}' already exists.")
+            raise typer.Exit(1)
+
+    save_assistant(assistant_data)
+    typer.echo(f"Assistant '{name}' registered.")
+
+
+@assistants_app.command("delete")
+def delete_assistant(
+    name: str = typer.Argument(..., help="Name of the assistant"),
+):
+    assistant_file = get_assistant_file_path(name)
+    if assistant_file.exists():
+        assistant_file.unlink()
+        typer.echo(
+            f"Assistant '{name}' deleted. Note: This only removes the reference to the assistant, not the actual assistant file."
+        )
+    else:
+        typer.echo(f"Assistant '{name}' not found.")
+
+
+@assistants_app.command("list")
+def list_assistants():
+    assistant_files = ASSISTANTS_DIR.glob("*.json")
+    if assistant_files:
+        console = Console()
+        table = Table(show_header=True, header_style="bold magenta")
+        table.add_column("Name", style="dim", width=30)
+        table.add_column("Path", style="cyan")
+
+        for assistant_file in assistant_files:
+            assistant_data = load_assistant_data(assistant_file.stem)
+            if assistant_data:
+                table.add_row(assistant_data.name, str(assistant_data.path))
+
+        console.print(table)
+    else:
+        typer.echo("No assistants found.")
+
+
 @assistants_app.command()
 def say(
     message,
@@ -48,9 +206,26 @@ def say(
         help="The thread name to send the message to. Set MARVIN_CLI_THREAD to provide a default.",
         envvar="MARVIN_CLI_THREAD",
     ),
+    assistant_name: str = typer.Option(
+        None,
+        "--assistant",
+        "-a",
+        help="The name of the assistant to use. Set MARVIN_CLI_ASSISTANT to provide a default.",
+        envvar="MARVIN_CLI_ASSISTANT",
+    ),
 ):
     thread_data = threads_cli.get_or_create_thread(name=thread)
-    default_assistant.say(message, thread=Thread(id=thread_data.id), model=model)
+
+    if assistant_name:
+        try:
+            assistant = load_assistant(assistant_name)
+        except Exception as exc:
+            typer.echo(exc)
+            raise typer.Exit(1)
+    else:
+        assistant = default_assistant
+
+    assistant.say(message, thread=Thread(id=thread_data.id), model=model)
 
 
 if __name__ == "__main__":
