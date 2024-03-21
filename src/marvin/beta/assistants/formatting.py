0740c067ccb1feed71973773b9185b0e6df37ec6
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2024-03-21 09:52:32 -0400

    Refactor formatting

--- src/marvin/beta/assistants/formatting.py
+++ src/marvin/beta/assistants/formatting.py
@@ -3,6 +3,7 @@ import inspect
 import json
 import tempfile
 from datetime import datetime
+from typing import Optional
 
 from openai.types.beta.threads import Message
 from openai.types.beta.threads.runs.run_step import RunStep
@@ -17,146 +18,182 @@ from marvin.utilities.openai import get_openai_client
 json_parser = JSONParser()
 
 
+@functools.lru_cache(maxsize=1000)
+def download_temp_file(file_id: str, suffix: str = None):
+    """
+    Downloads a file from OpenAI's servers and saves it to a temporary file.
+
+    Args:
+        file_id: The ID of the file to be downloaded.
+        suffix: The file extension to be used for the temporary file.
+
+    Returns:
+        The file path of the downloaded temporary file.
+    """
+
+    client = get_openai_client(is_async=False)
+    response = client.files.content(file_id)
+
+    # Create a temporary file with a context manager to ensure it's cleaned up
+    # properly
+    temp_file = tempfile.NamedTemporaryFile(delete=False, mode="wb", suffix=suffix)
+    temp_file.write(response.content)
+
+    return temp_file.name
+
+
+def format_timestamp(timestamp: int) -> str:
+    return datetime.fromtimestamp(timestamp).strftime("%l:%M:%S %p")
+
+
+def create_panel(content: str, title: str, timestamp: int, color: str):
+    return Panel(
+        Markdown(inspect.cleandoc(content)),
+        title=f"[bold]{title}[/]",
+        subtitle=f"[italic]{format_timestamp(timestamp)}[/]",
+        title_align="left",
+        subtitle_align="right",
+        border_style=color,
+        box=box.ROUNDED,
+        width=100,
+        expand=True,
+        padding=(1, 2),
+    )
+
+
+def handle_code_interpreter(step, tool_call):
+    panel_title = "Code Interpreter"
+    footer = []
+    for output in tool_call.code_interpreter.outputs:
+        if output.type == "logs":
+            result, note = format_logs_output(output.logs)
+            footer_content = format_code_output(result, note)
+            footer.append(footer_content)
+        elif output.type == "image":
+            local_file_path = download_temp_file(output.image.file_id, suffix=".png")
+            footer.append(
+                f"The code interpreter produced this image: [{local_file_path}]({local_file_path})"
+            )
+    content = format_code_interpreter_input(tool_call.code_interpreter.input, footer)
+    return create_panel(content, panel_title, step.created_at, "gray74")
+
+
+def format_logs_output(logs):
+    if len(logs) > 500:
+        result = logs[:500] + " ..."
+        note = "*(First 500 characters shown)*"
+    else:
+        result = logs
+        note = ""
+    return result, note
+
+
+def format_code_output(result, note):
+    return inspect.cleandoc(
+        """
+        The code interpreter produced this result:
+
+        ```python
+        {result}
+        ```
+
+        {note}
+        """
+    ).format(result=result, note=note)
+
+
+def format_code_interpreter_input(input, footer):
+    footer = "\n".join(footer)
+    return inspect.cleandoc(
+        """
+        Running the code interpreter...
+
+        ```python
+        {input}
+        ```
+
+        {footer}
+        """
+    ).format(input=input, footer=footer)
+
+
+def handle_function_tool(step, tool_call):
+    panel_title = "Tool Call"
+    content = ""
+    if step.status == "in_progress":
+        if tool_call.function.arguments:
+            args = parse_function_arguments(tool_call.function.arguments)
+            content = format_function_call_input(tool_call.function.name, args)
+        else:
+            content = f"Using the `{tool_call.function.name}` tool."
+
+    if step.status == "completed" and tool_call.function.output:
+        result, note = format_function_output(tool_call.function.output)
+        content = format_function_result(tool_call.function.name, result, note)
+    return create_panel(content, panel_title, step.created_at, "gray74")
+
+
+def parse_function_arguments(arguments):
+    try:
+        return json.loads(arguments)
+    except json.JSONDecodeError:
+        try:
+            return json_parser.parse(arguments)
+        except Exception:
+            return arguments
+
+
+def format_function_call_input(function_name, args):
+    return inspect.cleandoc(
+        """
+        Using the `{function_name}` tool with these arguments:
+
+        ```python
+        {args}
+        ```
+        """
+    ).format(function_name=function_name, args=args)
+
+
+def format_function_output(output, length_cutoff: Optional[int] = 500):
+    if length_cutoff and len(output) > length_cutoff:
+        result = output[:length_cutoff] + " ..."
+        note = f"*(First {length_cutoff} characters shown)*"
+    else:
+        result = output
+        note = ""
+    return result, note
+
+
+def format_function_result(tool_name, result, note):
+    return inspect.cleandoc(
+        """
+        The `{tool_name}` tool produced this result:
+
+        ```python
+        {result}
+        ```
+
+        {note}
+        """
+    ).format(tool_name=tool_name, result=result, note=note)
+
+
 def format_step(step: RunStep) -> list[Panel]:
-    @functools.lru_cache(maxsize=1000)
+    @functools.lru_cache
     def _cached_format_step(_step):
-        """
-        Closure that allows for caching of the formatted step. "_step" is a
-        hashable identifier for the cache; the actual function reads the full
-        "step" from the parent scope.
-        """
-        # Timestamp formatting
-        timestamp = datetime.fromtimestamp(step.created_at).strftime("%l:%M:%S %p")
-
-        # default content
-        content = (
-            f"Assistant is performing an action: {step.type} - Status:"
-            f" {step.status}"
-        )
-
         panels = []
 
-        # attempt to customize content
         if step.type == "tool_calls":
             for tool_call in step.step_details.tool_calls:
                 if tool_call.type == "code_interpreter":
-                    panel_title = "Code Interpreter"
-                    footer = []
-                    for output in tool_call.code_interpreter.outputs:
-                        if output.type == "logs":
-                            content = inspect.cleandoc(
-                                """
-                                The code interpreter produced this result:
-                                
-                                ```python
-                                {result}
-                                ```
-                                
-                                {note}
-                                """
-                            )
-
-                            if len(output.logs) > 500:
-                                result = output.logs[:500] + " ..."
-                                note = "*(First 500 characters shown)*"
-                            else:
-                                result = output.logs
-                                note = ""
-                            footer.append(content.format(result=output.logs, note=note))
-                        elif output.type == "image":
-                            # Use the download_temp_file function to download the file and get
-                            # the local path
-                            local_file_path = download_temp_file(
-                                output.image.file_id, suffix=".png"
-                            )
-                            footer.append(
-                                f"The code interpreter produced this image: [{local_file_path}]({local_file_path})"
-                            )
-
-                    content = inspect.cleandoc(
-                        """
-                        Running the code interpreter...
-                        
-                        ```python
-                        {input}
-                        ```
-                        
-                        {footer}
-                        """
-                    ).format(
-                        input=tool_call.code_interpreter.input, footer="\n".join(footer)
-                    )
+                    panel = handle_code_interpreter(step, tool_call)
                 elif tool_call.type == "function":
-                    panel_title = "Tool Call"
-                    if step.status == "in_progress":
-                        if tool_call.function.arguments:
-                            try:
-                                args = json.loads(tool_call.function.arguments)
-                            except json.JSONDecodeError:
-                                try:
-                                    args = json_parser.parse(
-                                        tool_call.function.arguments
-                                    )
-                                except Exception:
-                                    args = tool_call.function.arguments
-
-                            content = inspect.cleandoc(
-                                """
-                                Using the `{function}` tool with these arguments:
-                                
-                                ```python
-                                {args}
-                                ```
-                                """
-                            ).format(function=tool_call.function.name, args=args)
-
-                        else:
-                            content = f"Assistant wants to use the `{tool_call.function.name}` tool."
-
-                    elif step.status == "completed":
-                        if tool_call.function.output:
-                            content = inspect.cleandoc(
-                                """
-                                The `{tool_name}` tool produced this result:
-                                
-                                ```python
-                                {result}
-                                ```
-                                
-                                {note}
-                                """
-                            )
-                            if len(tool_call.function.output) > 500:
-                                result = tool_call.function.output[:500] + " ..."
-                                note = "*(First 500 characters shown)*"
-                            else:
-                                result = tool_call.function.output
-                                note = ""
-                            content = content.format(
-                                tool_name=tool_call.function.name,
-                                result=result,
-                                note=note,
-                            )
-                        else:
-                            content = f"The `{tool_call.function.name}` tool has completed with no result."
-
-                # Create the panel for the run step status
-                panels.append(
-                    Panel(
-                        Markdown(inspect.cleandoc(content)),
-                        title=panel_title,
-                        subtitle=f"[italic]{timestamp}[/]",
-                        title_align="left",
-                        subtitle_align="right",
-                        border_style="gray74",
-                        box=box.ROUNDED,
-                        width=100,
-                        expand=True,
-                        padding=(1, 2),
-                    )
-                )
+                    panel = handle_function_tool(step, tool_call)
+                panels.append(panel)
 
         elif step.type == "message_creation":
+            # Handle message creation if necessary
             pass
 
         return Group(*panels)
@@ -191,41 +228,11 @@ def pprint_steps(steps: list[RunStep]):
         pprint_step(step)
 
 
-@functools.lru_cache(maxsize=1000)
-def download_temp_file(file_id: str, suffix: str = None):
-    """
-    Downloads a file from OpenAI's servers and saves it to a temporary file.
-
-    Args:
-        file_id: The ID of the file to be downloaded.
-        suffix: The file extension to be used for the temporary file.
-
-    Returns:
-        The file path of the downloaded temporary file.
-    """
-
-    client = get_openai_client(is_async=False)
-    response = client.files.content(file_id)
-
-    # Create a temporary file with a context manager to ensure it's cleaned up
-    # properly
-    temp_file = tempfile.NamedTemporaryFile(delete=False, mode="wb", suffix=suffix)
-    temp_file.write(response.content)
-
-    return temp_file.name
-
-
 def format_message(message: Message) -> Panel:
     role_colors = {
         "user": "green",
         "assistant": "blue",
     }
-
-    color = role_colors.get(message.role, "red")
-    timestamp = (
-        datetime.fromtimestamp(message.created_at).strftime("%I:%M:%S %p").lstrip("0")
-    )
-
     content = []
     for item in message.content:
         if item.type == "text":
@@ -242,18 +249,11 @@ def format_message(message: Message) -> Panel:
         content.append(f"Attached file: {file_id}\n")
 
     # Create the panel for the message
-    panel = Panel(
-        Markdown(inspect.cleandoc("\n\n".join(content))),
-        title=f"[bold]{message.role.capitalize()}[/]",
-        subtitle=f"[italic]{timestamp}[/]",
-        title_align="left",
-        subtitle_align="right",
-        border_style=color,
-        box=box.ROUNDED,
-        # highlight=True,
-        width=100,  # Fixed width for all panels
-        expand=True,  # Panels always expand to the width of the console
-        padding=(1, 2),
+    panel = create_panel(
+        "\n\n".join(content),
+        title=message.role.capitalize(),
+        timestamp=message.created_at,
+        color=role_colors.get(message.role, "red"),
     )
     return panel
 
