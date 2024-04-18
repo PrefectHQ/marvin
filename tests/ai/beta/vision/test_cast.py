0dec50acdb6ef56329fdd58a3d322e4e0fcd525d
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2024-04-17 23:54:43 -0400

    fix test

--- tests/ai/beta/vision/test_cast.py
+++ tests/ai/beta/vision/test_cast.py
@@ -13,8 +13,7 @@ def assert_locations_equal(observed: Location, expected: Location):
     assert_equal(
         observed,
         expected,
-        instructions="Do the locations refer to the same place even if the literal words differ? NYC, New York, and New York City are all the same place.",
-        model="gpt-3.5-turbo",
+        instructions="The location models may not be literally identical, but do they refer to the same city?",
     )
 
 
