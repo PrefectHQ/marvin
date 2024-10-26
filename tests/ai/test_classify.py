348f6163c542aebb92d25c7c548efc9589f6cdf3
zzstoatzz <thrast36@gmail.com>
2024-10-26 15:06:21 -0500

    stupid 3.5

--- tests/ai/test_classify.py
+++ tests/ai/test_classify.py
@@ -125,7 +125,7 @@ class TestClassify:
     class TestExamples:
         @pytest.mark.flaky(max_runs=3)
         async def test_hogwarts_sorting_hat(self):
-            description = "Brave, daring, chivalrous -- canonical Harry or James Potter"
+            description = "Brave, daring, chivalrous -- Ron, Harry, and Hermione"
 
             house = marvin.classify(
                 description,
