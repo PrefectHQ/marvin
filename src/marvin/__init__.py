84a2b72e1a32075a68f6360b83cb126de3f13c6f
zzstoatzz <thrast36@gmail.com>
2024-12-31 14:18:42 -0600

    define dunder all

--- src/marvin/__init__.py
+++ src/marvin/__init__.py
@@ -1,11 +1,41 @@
 from importlib.metadata import version as _version
 
 # necessary imports
+from marvin.settings import settings
 
 # core classes
+from marvin.engine.thread import Thread
+from marvin.agents.agent import Agent
+from marvin.tasks.task import Task
 
 # public functions
+from marvin.instructions import instructions
+from marvin.run import run, run_async, run_tasks_async
+from marvin.defaults import defaults, override_defaults
 
 # marvin fns
+from marvin.fns.classify import classify
+from marvin.fns.extract import extract
+from marvin.fns.cast import cast
+from marvin.fns.generate import generate
+from marvin.fns.fn import fn
 
 __version__ = _version("marvin")
+
+__all__ = [
+    "settings",
+    "Thread",
+    "Agent",
+    "Task",
+    "instructions",
+    "run",
+    "run_async",
+    "run_tasks_async",
+    "defaults",
+    "override_defaults",
+    "classify",
+    "extract",
+    "cast",
+    "generate",
+    "fn",
+]
