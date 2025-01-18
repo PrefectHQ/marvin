5972778c8ad17c9462b41326d8be06c5779a6b6a
zzstoatzz <thrast36@gmail.com>
2025-01-17 18:43:00 -0600

    simplify

--- tests/settings/test_settings_object.py
+++ tests/settings/test_settings_object.py
@@ -1,35 +1,30 @@
-import os
-
 import pytest
 
 from marvin.settings import Settings
 
 
-@pytest.fixture
-def current_user() -> str:
-    user = os.getenv("USER")
-    assert user is not None, "USER environment variable must be set"
-    return user
-
-
-def test_database_url_default(current_user: str):
+def test_database_url_default():
     settings = Settings()
-    assert settings.database_url == f"/Users/{current_user}/.marvin/marvin.db"
+    assert settings.database_url is not None
+    assert settings.database_url.endswith("/.marvin/marvin.db")
 
 
 @pytest.mark.parametrize(
-    "env_var_value, expected_database_url",
+    "env_var_value, expected_ending",
     [
         (":memory:", ":memory:"),
-        ("~/.marvin/test.db", "/Users/{user}/.marvin/test.db"),
+        ("~/.marvin/test.db", "/.marvin/test.db"),
     ],
 )
 def test_database_url_set_from_env_var(
     monkeypatch: pytest.MonkeyPatch,
     env_var_value: str,
-    expected_database_url: str,
-    current_user: str,
+    expected_ending: str,
 ):
     monkeypatch.setenv("MARVIN_DATABASE_URL", env_var_value)
     settings = Settings()
-    assert settings.database_url == expected_database_url.format(user=current_user)
+    if expected_ending == ":memory:":
+        assert settings.database_url == expected_ending
+    else:
+        assert settings.database_url is not None
+        assert settings.database_url.endswith(expected_ending)
