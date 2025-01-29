9ba1fa377f0a0cdbe03730e28c51eae604b2deb0
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2025-01-29 16:33:36 -0500

    Improve delegate handling

--- src/marvin/thread.py
+++ src/marvin/thread.py
@@ -119,6 +119,14 @@ class Thread:
         """Add a user message to the thread."""
         await self.add_messages_async([UserMessage(content=message)])
 
+    def add_info_message(self, message: str):
+        """Add an info message to the thread."""
+        return run_sync(self.add_info_message_async(message))
+
+    async def add_info_message_async(self, message: str) -> None:
+        """Add an info message to the thread."""
+        await self.add_messages_async([UserMessage(content=f"INFO MESSAGE: {message}")])
+
     def get_messages(
         self,
         before: datetime | None = None,
