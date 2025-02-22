1ae70e6a2c0c6a90e821ca0a3ffd1736ae5a6552
zzstoatzz <thrast36@gmail.com>
2025-02-21 20:26:27 -0600

    df example

--- examples/dataframe.py
+++ examples/dataframe.py
@@ -1,23 +1,31 @@
 # /// script
-# dependencies = ["pandas"]
+# dependencies = ["pandas", "marvin@git+https://github.com/prefecthq/marvin.git"]
 # ///
 
 """
 fills out a new column in a dataframe
 """
 
-import marvin
+from typing import Annotated
+
 import pandas as pd
+from pydantic import Field
 
+import marvin
 
-def transform_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+ColumnValue = Annotated[
+    str, Field(description="word for sum of the two numbers in the row")
+]
+
+if __name__ == "__main__":
+    df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+
     new_column_values = marvin.run(
-        result_type=list[str],
-        instructions="write the word for the sum of the two numbers in the row",
+        result_type=list[ColumnValue],
+        context={"a": df["a"], "b": df["b"]},
+        instructions="fill out a new column in the dataframe",
     )
-    new_column = pd.Series(
-    return df
+    new_column = pd.Series(new_column_values, dtype=str)
+    df["c"] = new_column
 
-
-df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
-
+    print(df)
