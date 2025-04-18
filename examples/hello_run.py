56bef820cbc738d30cd7cad4da6cd1f5114c3537
zzstoatzz <thrast36@gmail.com>
2025-04-17 21:27:03 -0500

    fix example

--- examples/hello_run.py
+++ examples/hello_run.py
@@ -8,6 +8,8 @@ import marvin
 class Location(TypedDict):
     name: str
     lat: Annotated[float, Field(ge=-90, le=90)]
+    lon: Annotated[float, Field(ge=-180, le=180)]
+
 
 print(
     marvin.run(
