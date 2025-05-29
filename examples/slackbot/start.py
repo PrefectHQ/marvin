add3f1b3f53e731044e3d16d7d37f6871f2b938b
zzstoatzz <thrast36@gmail.com>
2025-05-29 12:58:18 -0500

    secrets

--- examples/slackbot/start.py
+++ examples/slackbot/start.py
@@ -1,6 +1,15 @@
+import os
+
+from prefect.blocks.system import Secret
+
 if __name__ == "__main__":
     import uvicorn
     from settings import settings
+
+    if not (openai_api_key := os.getenv("OPENAI_API_KEY")):
+        os.environ["OPENAI_API_KEY"] = Secret.load(
+            settings.openai_api_key_secret_name, _sync=True
+        ).get()
 
     uvicorn.run(
         "api:app",
