3b58054f191dc71715e16a239fdfb91b37ff7793
zzstoatzz <thrast36@gmail.com>
2025-06-08 12:59:59 -0500

    slight fix

--- examples/slackbot/src/slackbot/__main__.py
+++ examples/slackbot/src/slackbot/__main__.py
@@ -11,16 +11,15 @@ if __name__ == "__main__":
     logger = get_logger(__name__)
     logger.debug(f"Starting Slackbot with model: {settings.model_name}")
 
-    if "gpt" in settings.model_name:
-        if not (openai_api_key := os.getenv("OPENAI_API_KEY")):
-            os.environ["OPENAI_API_KEY"] = Secret.load(
-                settings.openai_api_key_secret_name, _sync=True
-            ).get()
-    if "claude" in settings.model_name:
-        if not (anthropic_api_key := os.getenv("ANTHROPIC_API_KEY")):
-            os.environ["ANTHROPIC_API_KEY"] = Secret.load(
-                settings.anthropic_key_secret_name, _sync=True
-            ).get()
+    if not (openai_api_key := os.getenv("OPENAI_API_KEY")):  # Needed for embeddings
+        os.environ["OPENAI_API_KEY"] = Secret.load(
+            settings.openai_api_key_secret_name, _sync=True
+        ).get()
+
+    if not (anthropic_api_key := os.getenv("ANTHROPIC_API_KEY")):  # Needed for LLM
+        os.environ["ANTHROPIC_API_KEY"] = Secret.load(
+            settings.anthropic_key_secret_name, _sync=True
+        ).get()
 
     uvicorn.run(
         "slackbot.api:app",
