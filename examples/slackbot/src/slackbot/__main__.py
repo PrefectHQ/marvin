33c0056626d635544981e1b533a0caab0d39dd10
zzstoatzz <thrast36@gmail.com>
2025-06-08 13:07:56 -0500

    annoying

--- examples/slackbot/src/slackbot/__main__.py
+++ examples/slackbot/src/slackbot/__main__.py
@@ -11,7 +11,6 @@ if __name__ == "__main__":
     logger = get_logger(__name__)
     logger.debug(f"Starting Slackbot with model: {settings.model_name}")
 
-
     if not (openai_api_key := os.getenv("OPENAI_API_KEY")):  # Needed for embeddings
         os.environ["OPENAI_API_KEY"] = Secret.load(
             settings.openai_api_key_secret_name, _sync=True
