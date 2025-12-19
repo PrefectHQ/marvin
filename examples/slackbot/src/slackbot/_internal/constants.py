f0bee183c879678383949dfaac326bcf70a47b03
nate nowack <thrast36@gmail.com>
2025-12-19 12:35:32 -0600

    remove channel restrictions from slackbot (#1254)
    allow marvin to respond in any channel, not just designated ones

--- examples/slackbot/src/slackbot/_internal/constants.py
+++ examples/slackbot/src/slackbot/_internal/constants.py
@@ -1,4 +1,1 @@
-WORKSPACE_TO_CHANNEL_ID = {
-    "TAN3D79AL": "C046WGGKF4P",  # Prefect -> #ask-marvin-tests
-    "T03S63K44P6": "C03S3HZ2X3M",  # Stoat LLC -> #testing-slackbots
-}
+WORKSPACE_TO_CHANNEL_ID: dict[str, str] = {}
